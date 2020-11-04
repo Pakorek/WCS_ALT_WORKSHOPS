95ff0c1b6aa2362a98bdaba07b3ce46324a676e3
pakorek <brice.darmenia@sfr.fr>
2020-11-04 19:28:29 +0100

    refact controller with Async / Await

--- 01_API_REST/controllers/wilders.js
+++ 01_API_REST/controllers/wilders.js
@@ -1,40 +1,49 @@
 const WilderModel = require("../models/Wilder.js")
 
-exports.create = (req, res) => {
-    WilderModel.init().then( () => {
-            const newWilder = new WilderModel(req.body)
-            newWilder.save()
-                .then(() => res.status(200).json({ message: "Wilder créé !"}))
-                .catch(error => res.status(400).json({ error }))
-        }
-    )
-        .catch(error => res.status(400).json({ error }))
+exports.create = async (req, res) => {
+    try {
+        await WilderModel.init()
+        const newWilder = new WilderModel(req.body)
+        const result = await newWilder.save()
+        res.status(200).json({ success: true, result })
+    } catch (error) {
+        res.status(400).json({ success: false, error })
+    }
 }
 
-exports.modifyWilder = (req, res) => {
-    WilderModel.findOne({ name: req.params.name })
-        .then(result => {
-            WilderModel.updateOne({ _id: result._id}, {...req.body})
-                .then(() => res.status(200).json({ success: true, message: "Wilder mis à jour"}))
-                .catch(error => res.status(400).json({ error }))
-        })
-        .catch(error => res.status(400).json({ error }))
+exports.modifyWilder = async (req, res) => {
+    try {
+        const result = await WilderModel.findOne({ name: req.params.name })
+        await WilderModel.updateOne({ _id: result._id}, {...req.body})
+        res.status(200).json({ success: true, message: "Wilder mis à jour"})
+    } catch (error) {
+        res.status(400).json({ success: false, error })
+    }
 }
 
-exports.findOne = (req, res) => {
-    WilderModel.findOne({ name: req.params.name })
-        .then((result) => res.status(200).json({ result }))
-        .catch(error => res.status(400).json({ error }))
+exports.findOne = async (req, res) => {
+    try {
+        const result = await WilderModel.findOne({ name: req.params.name })
+        res.status(200).json({ result })
+    } catch (error) {
+        res.status(400).json({ success: false, error })
+    }
 }
 
-exports.findAll = (req, res) => {
-    WilderModel.find()
-        .then(result => res.status(200).json({ result }))
-        .catch(error => res.status(400).json({ error }))
+exports.findAll = async (req, res) => {
+    try {
+        const result = await WilderModel.find()
+        res.status(200).json({ result })
+    } catch (error) {
+        res.status(400).json({ error })
+    }
 }
 
-exports.remove = (req, res) => {
-    WilderModel.deleteOne({ name: req.params.name })
-        .then(() => res.status(200).json({ message : "Wilder supprimé" }))
-        .catch(error => res.status(400).json({ error }))
+exports.remove = async (req, res) => {
+    try {
+        await WilderModel.deleteOne({ name: req.params.name })
+        res.status(200).json({ success: true, message : "Wilder supprimé" })
+    } catch (error) {
+        res.status(400).json({ error })
+    }
 }
